8ffd639c319ac12acccb6abf0037ef5dfb7526e2
nicholaswell <89791991+nicholaswell@users.noreply.github.com>
2021-10-04 00:22:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,82 @@
-var box = document.body.querySelector(".box")
+var wrapperEl = document.body.querySelector(".wrapper")
 
-var n1 = Number(prompt("Enter a number"))
-var op = String(prompt("Enter an operator: +,-,/,*"))
-var n2 = Number(prompt("Enter another number"))
+var warriors = [
+    {
+        name:"Bob",
+        damage:2,
+        health:10,
+        warrior:true
+    },
+    {
+        name:"Jerry",
+        damage:1,
+        health:12,
+        warrior:true
+    },
+    {
+        name:"Mavis",
+        damage:2,
+        health:10,
+        warrior:true
+    },
+    {
+        name:"Morty",
+        damage:4,
+        health:10,
+        warrior:true
+    },
+    {
+        name:"Shorty",
+        damage:10,
+        health:3,
+        warrior:false
+    },
+    {
+        name:"Porty",
+        damage:1,
+        health:14,
+        warrior:true
+    },{
+        name:"Perry",
+        damage:2,
+        health:9,
+        warrior:true
+    },
+    {
+        name:"Larry",
+        damage:2,
+        health:17,
+        warrior:false
+    }
+]
 
-if(isNaN(n1)) {
-    box.innerHTML = "Invalid number 1"
-} else if(op !== "+" && op !== "-" && op !== "/" && op !== "*") {
-    box.innerHTML = "Invalid operator"
-} else if(isNaN(n2)) {
-    box.innerHTML = "Invalid number 2"
-} else{
-    if(op == "+"){
-        box.innerHTML = "Your answer is: " + Number(n1+n2)
-    }
-    if(op == "-"){
-        box.innerHTML = "Your answer is: " + Number(n1-n2)
-    }
-    if(op == "/"){
-        box.innerHTML = "Your answer is: " + Number(n1/n2)
-    }
-    if(op == "*"){
-        box.innerHTML = "Your answer is: " + Number(n1*n2)
-    }
+for(var i = 0; i < warriors.length; i++){
+    var el = document.createElement("div")
+    var nameEl = document.createElement("h2")
+    var damageEl = document.createElement("h4")
+    var healthEl = document.createElement("h4")
+    if(warriors[i].health >= 10){
+        if(warriors[i].damage >= 2){
+            if(warriors[i].warrior === true){
+                    if((warriors[i].name).includes("a") || (warriors[i].name).includes("A")){
+                        nameEl.innerHTML="Name: " + (warriors[i].name).fontcolor("red")
+                    } else {
+                        nameEl.innerHTML = "Name: " + warriors[i].name
+                    }
+                        damageEl.innerHTML = "Damage: " + warriors[i].damage
+                        healthEl.innerHTML = "Health: " + warriors[i].health
+
+                        el.appendChild(nameEl)
+                        el.appendChild(damageEl)
+                        el.appendChild(healthEl)
+                        wrapperEl.appendChild(el)
+
+                }
+            }
+        }
+
+
+
+
+
 }
-
-
-
-
-
